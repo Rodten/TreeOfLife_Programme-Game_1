0aa64d26efc4a9b7e67fba5c3dc2e4467ec410e5
Rodten <hfdonthework@gmail.com>
2023-03-26 15:22:13 +0700

    Camera Movement

---
+++ script/MouseCamera.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+
+public class MouseCamera : MonoBehaviour
+{
+    private Camera cam;
+    [SerializeField] private float minFov = 20;
+    [SerializeField] private float maxFov = 80;
+
+    void Awake()
+    {
+        cam = GetComponent<Camera>();
+        // ensures that the field of view is within min and max on startup
+        UpdateFov(cam.fieldOfView);
+    }
+
+    void Update()
+    {
+        float scrollInput = Input.GetAxis("Mouse ScrollWheel");
+        if (scrollInput > 0)
+            UpdateFov(cam.fieldOfView - 1);
+        else if (scrollInput < 0)
+            UpdateFov(cam.fieldOfView + 1);
+    }
+
+    void UpdateFov(float newFov)
+    {
+        cam.fieldOfView = Mathf.Clamp(newFov, minFov, maxFov);
+    }
+}
